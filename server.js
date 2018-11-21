14c918fb281d1fcd64fd105214b27371e810c1e8
thenath1998 <rkz.suman@gmail.com>
2018-11-22 02:12:15 +0530

    removing maintenance

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use((req, res, next) => {
-    res.render('maintenance.hbs', {
-        pageTitle: 'Maintenance'
-    });
-});
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs', {
+//         pageTitle: 'Maintenance'
+//     });
+// });
 
 app.use(express.static(__dirname + '/public'));
 
